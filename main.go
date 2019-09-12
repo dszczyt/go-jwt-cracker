3ac8b4d26f9e8cf5e10bf4087f221597fa4ddeca
Damien Szczyt <damien.szczyt@geniustrade.com>
2019-09-12 10:11:53 +0200

    fix issue

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func JWTFromString(s string) (*JWT, error) {
 
 func (jwt *JWT) isSignedWithKey(key []byte) bool {
 	mac := hmac.New(sha256.New, key)
-	if err := mac.Write([]byte(jwt.signedContent)); err != nil {
+	if _, err := mac.Write([]byte(jwt.signedContent)); err != nil {
 		panic(err)
 	}
 	expectedMac := mac.Sum(nil)
